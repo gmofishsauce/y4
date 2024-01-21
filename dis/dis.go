599c71ac92ab69920b8749b92a9aa7cb915cc1a6
gmofishsauce <pdxjjb@gmail.com>
2024-01-21 12:53:40 -0800

    Updates to assembler asm, disassembler dis, and the instruction test framework itf to conform with latest architecture and ISA document.

--- dis/dis.go
+++ dis/dis.go
@@ -45,7 +45,7 @@ const SRX uint16 = 4 // special, register
 const RRX uint16 = 5 // register, register
 const RXX uint16 = 6 // register
 const XXX uint16 = 7 // no arguments
-const RHI uint16 = 8 // register, imm3, imm7 (jlr only)
+const RHI uint16 = 8 // register, imm3, imm7 (jlr only/special case)
 
 // Names of the registers, indexed by field content
 var RegNames []string = []string {
@@ -54,7 +54,7 @@ var RegNames []string = []string {
 
 // Names of special registers, indexed by field content
 var SprNames []string = []string {
-	"psw", "lnk", "pc", "err3", "err4", "err5", "err6", "err7",
+	"lnk", "pc", "err3", "err4", "err5", "err6", "err7",
 }
 
 // The allowed mnemonics and their signatures. This table is
@@ -70,7 +70,7 @@ var KeyTable []KeyEntry = []KeyEntry {
 	{"beq", 3,  0x8000, RRI},
 	{"adi", 3,  0xA000, RRI},
 	{"lui", 3,  0xC000, RJX},
-	{"jlr", 4,  0xE000, RHI}, // reg, 3-bit imm, 7-bit positive imm
+	{"BUG", 4,  0xE000, RHI}, // special case(s)
 
 	// 3-operand XOPs
 	{"add", 7,  0xF000, RRR},
@@ -82,13 +82,13 @@ var KeyTable []KeyEntry = []KeyEntry {
 	{"xor", 7,  0xFC00, RRR},
 
 	// 2 operand YOPs
-	{"wrs", 10, 0xFE00, SRX},
-	{"rds", 10, 0xFE40, SRX},
-	{"lds", 10, 0xFE80, SRX},
-	{"sts", 10, 0xFEC0, SRX},
-	{"y04", 10, 0xFF00, XXX},
-	{"ior", 10, 0xFF40, RRX},
-	{"iow", 10, 0xFF80, RRX},
+	{"lds", 10, 0xFE00, SRX},
+	{"sts", 10, 0xFE40, SRX},
+	{"rds", 10, 0xFE80, SRX},
+	{"wrs", 10, 0xFEC0, SRX},
+	{"ior", 10, 0xFF00, RRX},
+	{"iow", 10, 0xFF40, RRX},
+	{"y07", 10, 0xFF80, XXX},
 
 	// 1 operand ZOPs
 	{"not", 13, 0xFFC0, RXX},
@@ -100,13 +100,13 @@ var KeyTable []KeyEntry = []KeyEntry {
 	{"asr", 13, 0xFFF0, RXX},
 
 	// 0 operand VOPs
-	{"sys", 16, 0xFFF8, XXX},
-	{"srt", 16, 0xFFF9, XXX},
+	{"srt", 16, 0xFFF8, XXX},
+	{"rtl", 16, 0xFFF9, XXX},
 	{"v02", 16, 0xFFFA, XXX},
 	{"v03", 16, 0xFFFB, XXX},
-	{"rtl", 16, 0xFFFC, XXX},
-	{"hlt", 16, 0xFFFD, XXX},
-	{"brk", 16, 0xFFFE, XXX},
+	{"hlt", 16, 0xFFFC, XXX},
+	{"brk", 16, 0xFFFD, XXX},
+	{"v06", 16, 0xFFFE, XXX},
 	{"die", 16, 0xFFFF, XXX},
 }
 
@@ -135,21 +135,34 @@ func main() {
 }
 
 // Disassemble an object file. Files written by the assembler currently
-// have no header. There can be up to 128kb of code, but programs are
-// typically small because there's no compiler (yet). The opcode 0x0000
-// is "load word from memory to register 0". This is meaningless because,
-// like many RISC machines, r0 is a black hole. So the disassembler stops
-// when it sees an aligned 0 word.
+// have no header. They consist of up to four sections: user code (at 0,
+// length 128kB), user data (at 128k in file, length 64kB), kernel code
+// (at 192k in file, length 128kB), and kernel data (at 320k, length 64kB).
+// The disassembler ignores the data segments. It disassembles both code
+// segments with the appropriate pseudo instructions to align them (space
+// them at 0 and 192k). It stops processing the current segment if it sees
+// the illegal opcode 0 (which causes an illegal instruction trap). Since
+// there are no 16-bit immediate values in the ISA, this is reliable.
 
-const MaxCodeSegLen = 64*1024 // uints
+const K64 int64 = 64*1024
 
 func disassemble(f *os.File) error {
+	if err := disassembleSection(f, 0); err != nil {
+		return fmt.Errorf("user code section: %s", err.Error())
+	}
+	if err := disassembleSection(f, 3*K64); err != nil {
+		return fmt.Errorf("kernel code section: %s", err.Error())
+	}
+	return nil
+}
+
+func disassembleSection(f *os.File, pos int64) error {
 	var b []byte = make([]byte, 2, 2) 
 	var inst uint16
-	var at int
+	var at int64 // 16-bit instruction index in section
 	var err error
 
-	for n, err := f.Read(b); n == 2 && err == nil && at < MaxCodeSegLen; n, err = f.Read(b) {
+	for n, err := f.ReadAt(b, pos); n == 2 && err == nil && at < 2*K64; n, err = f.ReadAt(b, pos) {
 		if inst = binary.LittleEndian.Uint16(b[:]); inst == 0 {
 			break
 		}
@@ -159,6 +172,7 @@ func disassemble(f *os.File) error {
 			fmt.Printf("%5d: 0x%04X: %s\n", at, inst, decode(inst))
 		}
 		at++
+		pos += 2
 	}
 	if err != nil && !errors.Is(err, io.EOF) {
 		return err
@@ -196,8 +210,9 @@ func decode(op uint16) string {
 		args = fmt.Sprintf("%s, %s, 0x%02X",
 			RegNames[bits(op,2,0)], RegNames[bits(op,5,3)], bits(op,12,6))
 	case RHI:
-		args = fmt.Sprintf("%s, 0x%1X, 0x%02X",
-			RegNames[bits(op,2,0)], bits(op,5,3), bits(op,12,6))
+		// A bit ugly but in this case we have to return the whole instruction
+		// here, rather than just the args.
+		return decodeJLR(op)
 	case RJX:
 		args = fmt.Sprintf("%s, 0x%03X", RegNames[bits(op,2,0)], bits(op,12,3))
 	case RRR:
@@ -218,7 +233,31 @@ func decode(op uint16) string {
 	return fmt.Sprintf("%s %s", found.name, args)
 }
 
-// Hi and lo are inclusive bit numbers
+func decodeJLR(op uint16) string {
+	if bits(op,12,6) < 64 && (bits(op,12,6)&1) == 0 && bits(op,5,3) == 0 && bits(op,2,0) == 0 {
+		// The immediate is an even number < 64; ra and rb fields are both 0: system call trap
+		return fmt.Sprintf("sys %d", bits(op,12,6))
+	} else if bits(op,2,0) != 0 && bits(op,5,3) == 0 {
+		// The ra field is nonzero and the rb field is 0: conventional jump and link, called jsr
+		if bits(op,12,6) == 0 {
+			return fmt.Sprintf("jsr %s", RegNames[bits(op,2,0)])
+		} else {
+			return fmt.Sprintf("jsr %s, 0x%04X", RegNames[bits(op,2,0)], bits(op,12,6))
+		}
+	} else if bits(op,2,0) != 0 && bits(op,5,3) == 1 {
+		// The ra field is nonzero and the rb field is 1: conventional jump, called jmp
+		if bits(op,12,6) == 0 {
+			return fmt.Sprintf("jmp %s", RegNames[bits(op,2,0)])
+		} else {
+			return fmt.Sprintf("jmp %s, 0x%04X", RegNames[bits(op,2,0)], bits(op,12,6))
+		}
+	}
+	// It's not one of those cases - maybe it's a sys instruction with an odd offset
+	// or the rb field is > 1 - disassemble as die with a comment containing the opcode
+	return fmt.Sprintf("die ; ILLEGAL OPCODE 0x%04X", op)
+}
+
+// Hi and lo are inclusive bit numbers - "15,13" is the 3 MS bits of a uint16
 func bits(op uint16, hi uint16, lo uint16) uint16 {
 	b := hi - lo + 1 // if hi, low == 5,3 then b == 3
 	var mask uint16 = 1<<b - 1 // 1<<3 == 8 so mask == 7 == 0b111
--- itf/README.md
+++ itf/README.md
@@ -5,10 +5,11 @@ tripping them. It contains some test programs with the y4a (YARC-4 assembler)
 extension.
 
 For each test program t1.y4a, t2, etc., the itf program (itf.go) first
-assembles the test program, producing y4.out It moves the generated binary
+assembles the test program, producing y4.out. It moves the generated binary
 to a temporary working directory. The binary file is disassembled producing
-a disassembly, which is then assembled to producte another y4.out file.
-The two assembler output binaries are then compared for equality.
+a disassembly in the temporary directory, which is then assembled to produce
+another y4.out file. The two assembler output binaries are then compared for
+equality.
 
 This approach removes all issues with minor textual differences such as number
 bases between the original assembler source and the disassembled source.
